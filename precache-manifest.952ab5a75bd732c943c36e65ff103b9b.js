0fc876b58d95a17a5f1b80530b76409c64423b31
unknown <you@example.com>
2020-10-05 23:48:34 -0700

    Updates

---
+++ precache-manifest.952ab5a75bd732c943c36e65ff103b9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6b2e6b8da14a929f9ac7bd332f1219",
+    "url": "/git-user/index.html"
+  },
+  {
+    "revision": "8bbc5aeb42f8d2570c97",
+    "url": "/git-user/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "a2bcbb3e845b1923456f",
+    "url": "/git-user/static/css/main.487e18b4.chunk.css"
+  },
+  {
+    "revision": "8bbc5aeb42f8d2570c97",
+    "url": "/git-user/static/js/2.187c5e47.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/git-user/static/js/2.187c5e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2bcbb3e845b1923456f",
+    "url": "/git-user/static/js/main.87baa144.chunk.js"
+  },
+  {
+    "revision": "b3df943038a15db9b424",
+    "url": "/git-user/static/js/runtime-main.98083d35.js"
+  }
+]);
